6a60521e43f236231d58a6bbb70375ac27e885c7
Rafal Labisz <rafal0710@gmail.com>
2019-04-14 22:12:57 +0200

    Updates

---
+++ precache-manifest.00dfd958fecedb20ef7a3528c5ab56e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4f9a703222d96c5ed72ac1b94aeca4e",
+    "url": "/rest-api-app/static/media/image.b4f9a703.jpg"
+  },
+  {
+    "revision": "57378c8ca1f973b5f203",
+    "url": "/rest-api-app/static/js/runtime~main.d3180edc.js"
+  },
+  {
+    "revision": "4533d9f85c8f42b3962b",
+    "url": "/rest-api-app/static/js/main.a8e230de.chunk.js"
+  },
+  {
+    "revision": "f6b10aa4e2b83e499a38",
+    "url": "/rest-api-app/static/js/2.7cfd094c.chunk.js"
+  },
+  {
+    "revision": "4533d9f85c8f42b3962b",
+    "url": "/rest-api-app/static/css/main.8f9efafa.chunk.css"
+  },
+  {
+    "revision": "f6b10aa4e2b83e499a38",
+    "url": "/rest-api-app/static/css/2.f81da442.chunk.css"
+  },
+  {
+    "revision": "e8ab7bfa305f0658a84a17d3572757d5",
+    "url": "/rest-api-app/index.html"
+  }
+];
